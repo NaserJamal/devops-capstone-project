92bf86c1f51635cf664da080d39e85a0242d02e9
NaserJamal <naser.dll@hotmail.com>
2024-07-16 00:51:53 -0400

    Added Read Tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -49,6 +49,7 @@ class TestAccountService(TestCase):
     def tearDown(self):
         """Runs once after each test case"""
         db.session.remove()
+        
 
     ######################################################################
     #  H E L P E R   M E T H O D S
@@ -123,4 +124,33 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    # ADD YOUR TEST CASES HERE ...     
+
+    # def test_get_account_not_found(self):
+    #     """It should not Read an Account that is not found"""
+    #     resp = self.client.get(f"{BASE_URL}/0")
+    #     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)   
+
+    ######################################################################
+    # READ AN ACCOUNT
+    ######################################################################
+    @app.route("/accounts/<int:account_id>", methods=["GET"])
+    def get_accounts(account_id):
+        """
+        Reads an Account
+        This endpoint will read an Account based the account_id that is requested
+        """
+        app.logger.info("Request to read an Account with id: %s", account_id)
+
+        account = Account.find(account_id)
+        if not account:
+            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
+        return account.serialize(), status.HTTP_200_OK
+
+    # def test_get_account_not_found(self):
+    #     """It should not Read an Account that is not found"""
+    #     resp = self.client.get(f"{BASE_URL}/0")
+    #     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+

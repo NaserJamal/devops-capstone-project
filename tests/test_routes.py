8967d6a2684c38893a665794719b9ddf8ddeb793
NaserJamal <naser.dll@hotmail.com>
2024-07-16 01:20:45 -0400

    Added Delete Tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -126,10 +126,10 @@ class TestAccountService(TestCase):
 
     # ADD YOUR TEST CASES HERE ...
 
-    # def test_get_account_not_found(self):
-    #     """It should not Read an Account that is not found"""
-    #     resp = self.client.get(f"{BASE_URL}/0")
-    #     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_account(self):
         """It should Delete an Account"""
